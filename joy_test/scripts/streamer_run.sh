e7a5febc7d4ce878820c148c4b62a5fec952899b
Harsha Satya <robertsatya@gmail.com>
2015-10-09 16:39:19 +0000

    Change fps from 10 to 12

--- joy_test/scripts/streamer_run.sh
+++ joy_test/scripts/streamer_run.sh
@@ -1,1 +1,1 @@
-mjpg_streamer -i "/usr/local/lib/input_uvc.so -y -n -f 10 -r 640x480" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080"
+mjpg_streamer -i "/usr/local/lib/input_uvc.so -y -n -f 12 -r 640x480" -o "/usr/local/lib/output_http.so -w /usr/local/www -p 8080"
