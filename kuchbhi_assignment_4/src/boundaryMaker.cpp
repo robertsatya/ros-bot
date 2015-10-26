312320ca4a300fd7e486e0e0ba6ebaaade32a4ca
Harsha Satya <robertsatya@gmail.com>
2015-10-26 01:18:24 -0700

    Added code for creating bounding boxes

--- kuchbhi_assignment_4/src/boundaryMaker.cpp
+++ kuchbhi_assignment_4/src/boundaryMaker.cpp
@@ -1,90 +1,111 @@
 #include <ros/ros.h>
 #include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
-#include "opencv2/core/core.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
-#include "sensor_msgs/image_encodings.h"
+#include <sensor_msgs/image_encodings.h>
 
 using namespace cv;
 using namespace std;
 
+void thresh_callback(int, void* );
+
+static const std::string SOURCE_WINDOW = "Source Window";
+static const std::string BOUNDS_WINDOW = "Boundary Window";
 Mat src; Mat src_gray;
 int thresh = 100;
 int max_thresh = 255;
 RNG rng(12345);
 
-/// Function header
-void thresh_callback(int, void* );
+class BoundaryMaker
+{
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+  int color_mode_;
 
-/**
- * @function main
- */
-int main( int argc, char** argv )
+  public:
+    BoundaryMaker(int color_mode_) : it_(nh_)
+    {
+      image_sub_ = it_.subscribe("/camera/image_raw", 1, &BoundaryMaker::imageCb, this);
+      image_pub_ = it_.advertise("/image_converter/output_video", 1);
+      this->color_mode_ = color_mode_;
+      cv::namedWindow(SOURCE_WINDOW, WINDOW_AUTOSIZE);
+    }
+
+    ~BoundaryMaker()
+    {
+      cv::destroyWindow(SOURCE_WINDOW);
+    }
+
+    void imageCb(const sensor_msgs::ImageConstPtr& msg)
+    {
+      cv_bridge::CvImagePtr cv_ptr;
+      try
+      {
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      }
+      catch (cv_bridge::Exception& e)
+      {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+      }
+
+      src = cv_ptr->image;
+      cvtColor( src, src_gray, CV_BGR2GRAY );
+      GaussianBlur( src_gray, src_gray, Size(51,51), 0, 0 );
+
+      createTrackbar( " Threshold:", BOUNDS_WINDOW, &thresh, max_thresh, thresh_callback );
+      thresh_callback(0, 0);
+
+      cv::imshow(SOURCE_WINDOW, cv_ptr->image);
+      cv::waitKey(3);
+    }
+};
+
+int main(int argc, char** argv)
 {
   ros::init(argc, argv, "boundaryMaker");
-  ros::NodeHandle bn;
-
-  /// Load source image and convert it to gray
-  src = imread( argv[1], 1 );
-
-  /// Convert image to gray and blur it
-  cvtColor( src, src_gray, COLOR_BGR2GRAY );
-  blur( src_gray, src_gray, Size(3,3) );
-
-  /// Create Window
-  const char* source_window = "Source";
-  namedWindow( source_window, WINDOW_AUTOSIZE );
-  imshow( source_window, src );
-
-  createTrackbar( " Threshold:", "Source", &thresh, max_thresh, thresh_callback );
-  thresh_callback( 0, 0 );
-
-  waitKey(0);
-  return(0);
+  BoundaryMaker ic1(0);
+  ros::spin();
+  return 0;
 }
 
-/**
- * @function thresh_callback
- */
 void thresh_callback(int, void* )
 {
   Mat threshold_output;
   vector<vector<Point> > contours;
   vector<Vec4i> hierarchy;
 
-  /// Detect edges using Threshold
   threshold( src_gray, threshold_output, thresh, 255, THRESH_BINARY );
-  /// Find contours
-  findContours( threshold_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0) );
+  findContours( threshold_output, contours, hierarchy, RETR_CCOMP, CHAIN_APPROX_TC89_KCOS, Point(0, 0) );
 
-  /// Approximate contours to polygons + get bounding rects and circles
   vector<vector<Point> > contours_poly( contours.size() );
   vector<Rect> boundRect( contours.size() );
   vector<Point2f>center( contours.size() );
   vector<float>radius( contours.size() );
 
   for( size_t i = 0; i < contours.size(); i++ )
-     { approxPolyDP( Mat(contours[i]), contours_poly[i], 3, true );
-       boundRect[i] = boundingRect( Mat(contours_poly[i]) );
-       minEnclosingCircle( contours_poly[i], center[i], radius[i] );
-     }
+  {
+    approxPolyDP( Mat(contours[i]), contours_poly[i], 3, false );
+    boundRect[i] = boundingRect( Mat(contours_poly[i]) );
+    minEnclosingCircle( contours_poly[i], center[i], radius[i] );
+  }
 
-
-  /// Draw polygonal contour + bonding rects + circles
   Mat drawing = Mat::zeros( threshold_output.size(), CV_8UC3 );
   for( size_t i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
-       rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
-       circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
-     }
+  {
+    Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+    drawContours( drawing, contours_poly, (int)i, color, 1, 8, vector<Vec4i>(), 0, Point() );
+    rectangle( drawing, boundRect[i].tl(), boundRect[i].br(), color, 2, 8, 0 );
+    circle( drawing, center[i], (int)radius[i], color, 2, 8, 0 );
+  }
 
-  /// Show in a window
-  namedWindow( "Contours", WINDOW_AUTOSIZE );
-  imshow( "Contours", drawing );
+  namedWindow( BOUNDS_WINDOW, WINDOW_AUTOSIZE );
+  imshow( BOUNDS_WINDOW, drawing );
 }
